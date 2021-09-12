5fec0adaa98f7bb7626b2f3133474545b2c3aee4
evaristocuesta <evaristocg@gmail.com>
2021-09-12 20:34:59 +0200

    Edit and delete book added

--- angular/src/app/book/book.component.ts
+++ angular/src/app/book/book.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 import { BookService, BookDto, bookTypeOptions } from '@proxy/books';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { NgbDateNativeAdapter, NgbDateAdapter } from '@ng-bootstrap/ng-bootstrap';
+import { ConfirmationService, Confirmation } from '@abp/ng.theme.shared';
 
 @Component({
   selector: 'app-book',
@@ -16,6 +17,8 @@ import { NgbDateNativeAdapter, NgbDateAdapter } from '@ng-bootstrap/ng-bootstrap
 export class BookComponent implements OnInit {
   book = { items: [], totalCount: 0 } as PagedResultDto<BookDto>;
 
+  selectedBook = {} as BookDto;
+
   form: FormGroup; 
 
   // add bookTypes as a list of BookType enum members
@@ -26,7 +29,8 @@ export class BookComponent implements OnInit {
   constructor(
     public readonly list: ListService, 
     private bookService: BookService, 
-    private fb: FormBuilder) {}
+    private fb: FormBuilder, 
+    private confirmation: ConfirmationService) {}
 
   ngOnInit() {
     const bookStreamCreator = (query) => this.bookService.getList(query);
@@ -38,16 +42,28 @@ export class BookComponent implements OnInit {
 
   
   createBook() {
+    this.selectedBook = {} as BookDto;
     this.buildForm(); // add this line
     this.isModalOpen = true;
   }
 
+  editBook(id: string) {
+    this.bookService.get(id).subscribe((book) => {
+      this.selectedBook = book;
+      this.buildForm();
+      this.isModalOpen = true;
+    });
+  }
+
   buildForm() {
     this.form = this.fb.group({
-      name: ['', Validators.required],
-      type: [null, Validators.required],
-      publishDate: [null, Validators.required],
-      price: [null, Validators.required],
+      name: [this.selectedBook.name || '', Validators.required],
+      type: [this.selectedBook.type || null, Validators.required],
+      publishDate: [
+        this.selectedBook.publishDate ? new Date(this.selectedBook.publishDate) : null,
+        Validators.required,
+      ],
+      price: [this.selectedBook.price || null, Validators.required],
     });
   }
 
@@ -56,10 +72,22 @@ export class BookComponent implements OnInit {
       return;
     }
 
-    this.bookService.create(this.form.value).subscribe(() => {
+    const request = this.selectedBook.id
+      ? this.bookService.update(this.selectedBook.id, this.form.value)
+      : this.bookService.create(this.form.value);
+
+    request.subscribe(() => {
       this.isModalOpen = false;
       this.form.reset();
       this.list.get();
+    });
+  }
+
+  delete(id: string) {
+    this.confirmation.warn('::AreYouSureToDelete', '::AreYouSure').subscribe((status) => {
+      if (status === Confirmation.Status.confirm) {
+        this.bookService.delete(id).subscribe(() => this.list.get());
+      }
     });
   }
 }
